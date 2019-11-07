f33a0b832cf798db80142c08cdae8421bdbcf51a
egconley <31393715+egconley@users.noreply.github.com>
2019-11-07 13:45:07 -0800

    added place_id from google api to table and Location constructor

--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS location_table;
 CREATE TABLE location_table (
     id SERIAL PRIMARY KEY,
     latitude FLOAT,
-    longitude FLOAT
+    longitude FLOAT,
+    place_id VARCHAR(255)
 )
